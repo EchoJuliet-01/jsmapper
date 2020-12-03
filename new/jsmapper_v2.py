3b5559e0100f0ee1b0f9fa8bcb4f502465ee917c
Jeff Francis <jeff@gritch.org>
2020-12-03 09:08:31 -0700

    cleaner than it was

--- new/jsmapper_v2.py
+++ new/jsmapper_v2.py
@@ -17,10 +17,29 @@ last_info=time.time()
 last_sent_pir=last_info
 ack=False
 grid="DM79"
-my_call="N0GQ"
+my_call="N0DUH"
+avg_pir_interval=300
+avg_info_wait=300
+max_age=1890
+
+pir_interval=random.randint(int(avg_pir_interval*0.75),int(avg_pir_interval*1.25))
+info_wait=random.randint(int(avg_info_wait*0.75),int(avg_info_wait*1.25))
 
 s.connect((host,port))
 s.settimeout(1)
+
+def best_station(stations,max_age):
+    if(len(a_stations)>0):
+        now=time.time()
+        best_call=False
+        best_snr=-99999
+        for key in list(stations.keys()):
+            if((stations[key][0]>best_snr) and (now-stations[key][1]<=max_age)):
+                best_call=key
+                best_snr=stations[key][0]
+        return(best_call)
+    else:
+        return(False)
 
 def send_message(message):
     global s
@@ -70,15 +89,16 @@ def rx_thread(name):
                         if((tmp[1]==my_call) and (tmp[2]=="ACK")):
                             ack=True
                     if("/A" in from_call):
-                        a_stations[from_call]=[snr,time.time()]
+                        a_stations[(from_call.split('/')[0]).strip()]=[snr,time.time()]
                     if(("/A" in from_call) and ("{" in value) and ("}" in value)):
                         print("Got info")
                         now=time.time()
                         last_info=now
                         last_sent_pir=now
-                        a_stations[from_call]=[snr,time.time()]
+                        a_stations[(from_call.split('/')[0]).strip()]=[snr,time.time()]
                         info=json.loads(value[value.find('{'):value.find('}')+1])
-        except socket.timeout:
+#        except socket.timeout:
+        except:
             n=n+1
 
 thread1 = Thread(target=rx_thread, args=("RX Thread",))
@@ -87,19 +107,20 @@ thread1.start()
 while True:
     time.sleep(5)
     now=time.time()
+    best=best_station(a_stations,max_age)
     if(info):
         print("I have the info I need: " + json.dumps(info))
     else:
         if(len(a_stations)>0):
-            print("I do not have the info I need to generate a PIR. If I don't receive it in " + str(120-int(now-last_info)) + " seconds, I'm going to ask for it.")
+            print("I do not have the info I need to generate a PIR. If I don't receive it in " + str(info_wait-int(now-last_info)) + " seconds, I'm going to ask for it.")
         else:
             print("I do not have the info I need to generate a PIR, and I have nobody to ask for it.")
-    if(len(a_stations)>0):
-        print("My Aggregation Station is: " + list(a_stations.keys())[0].split('/')[0])
+    if(best):
+        print("My Aggregation Station is: " + best)
     else:
         print("I have no Aggregation Station to talk to.")
     if(info):
-        print("I am sending my PIR in " + str(300-int(now-last_sent_pir)) + " seconds.")
+        print("I am sending my PIR in " + str(pir_interval-int(now-last_sent_pir)) + " seconds.")
     else:
         print("I cannot send my PIR.")
     if(ack):
@@ -107,10 +128,11 @@ while True:
     else:
         print("I did not get my last ACK back (yet).")
     print()
-    if(not(info) and (now>=last_info+120) and (len(a_stations)>0)):
+    if(not(info) and (now>=last_info+info_wait) and (best)):
         last_info=now
-        send_message(list(a_stations.keys())[0].split('/')[0] + " info?")
-    if((info) and (now>=last_sent_pir+300) and (len(a_stations)>0)):
+        send_message(best + " info?")
+    if((info) and (now>=last_sent_pir+pir_interval) and (best)):
         last_sent_pir=now
+        pir_interval=random.randint(int(avg_pir_interval*0.75),int(avg_pir_interval*1.25))
         ack=False
-        send_message("msg " + list(a_stations.keys())[0].split('/')[0] + " " + grid + ";PIR1=" + ['R','Y','G','U'][random.randint(0,3)])
+        send_message("msg " + best + " " + grid + ";PIR1=" + ['R','Y','G','U'][random.randint(0,3)])
